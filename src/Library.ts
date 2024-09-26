8f89f3a4089570a7cb945dad6427b82062d35f18
MANTHAN KHAJURIA <manshuu736@gmail.com>
2024-09-26 18:26:04 +0530

    Created the addBook function in Library Class to add new book

--- src/Library.ts
+++ src/Library.ts
@@ -9,7 +9,11 @@ type Book = {
   class Library {
     private books: Book[] = [];
   
-    
+    // Add a new book to the library
+    addBook(isbn: string, title: string, author: string, year: number): void {
+        const book: Book = { isbn, title, author, year, isAvailable: true };
+        this.books.push(book);
+      }
 
     viewAvailableBooks(): Book[] {
         return this.books.filter((book) => book.isAvailable);
